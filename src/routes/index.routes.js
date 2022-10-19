1ca47455bd4cac900abaa6ca381f89f24cca0b62
CHARLY_BGOOD <bonavita.carlos@gmail.com>
2022-10-19 10:27:58 -0300

    modify style

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,8 +1,15 @@
 import { Router } from "express";
 const router = Router();
 
-router.get("/", (req, res) => res.render("index", {title: "objeto mediante express", x: "este valor que viene del backend, desde el index.routes"}));
-router.get("/about", (req, res) => res.render("about", {title: "About me"}));
-router.get("/contact", (req, res) => res.render("contact", {title: "Contact me"}));
+router.get("/", (req, res) =>
+  res.render("index", {
+    title: "objeto mediante express",
+    x: "este valor que viene del backend, desde el index.routes",
+  })
+);
+router.get("/about", (req, res) => res.render("about", { title: "About me" }));
+router.get("/contact", (req, res) =>
+  res.render("contact", { title: "Contact me" })
+);
 
 export default router;
